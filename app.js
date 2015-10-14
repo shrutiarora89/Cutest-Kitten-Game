096a5265492ff2c3bdfed038662f6c5fb03572ae
Shruti Arora <shruti.arora89@icloud.com>
2015-10-14 12:28:59 -0700

    minor changes

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-
 ////////////////////////CONSTRUCTOR//////////////////////////////////////////////////////
 
 // index starts with 0 to 11
 var kittyList = [];  // kittyList Empty Array
 
+var arrayOfZeros = [];
+
 // Constructor variable CuteKitty starts with a CAPITAL Letter
-var CuteKitty = function(images) {
-  this.images = images; //property
+var CuteKitty = function(source) {
+  this.source = source; //property
+  this.count=0;
+
 
   this.addImage=function(){ //method to push images in kittyList Array
-    kittyList.push(this.images);
+    kittyList.push(this);
+  }
+
+  this.addAnotherArray=function(){
+    arrayOfZeros.push(this.count);
   }
 
   this.addImage(); //Calling the adddImage Function
+  this.addAnotherArray();
 }
 
-/////////////////////////////////INSTANCES//////////////////////////
-var catCup = new CuteKitty('<img src="img/cat-cup.jpg" id="cat-cup"class="size">');
-var hands = new CuteKitty('<img src="img/hands.jpg" id="hands" class="size">');
-var hangingWood = new CuteKitty('<img src="img/hanging-wood-kitten.jpg" id="hanging-wood-kitten" class="size">');
-var lookingDown =new CuteKitty('<img src="img/looking-down.jpg" id="looking-down" class="size">');
-var tiaraKitten =new CuteKitty('<img src="img/tiara-kitten.jpg" id="tiara-kitten" class="size">');
-var pinkKitten =new CuteKitty('<img src="img/pink-kitten.jpg" id="pink-kitten" class="size">');
-var starbucksKitten =new CuteKitty('<img src="img/starbucks-kitten.jpg" id="starbucks-kitten" class="size">');
-var streching =new CuteKitty('<img src="img/streching.jpg" id="streching" class="size">');
-var plzKitten =new CuteKitty('<img src="img/plz-kitten.jpg" id="plz-kitten" class="size">');
-var cameraKitten =new CuteKitty('<img src="img/camera-kitten.jpg" id="camera-kitten" class="size">');
-var wrapJeans =new CuteKitty('<img src="img/wrap-jeans.jpg" id="wrap-jeans" class="size">');
-var specsKitten =new CuteKitty('<img src="img/specs-kitten.jpg" id="specs-kitten" class="size">');
-//////////////////////////////Random Two Images/////////////////////////////////////////
-
-
+/////////////////////////////////INSTANCES//////////////////////////////////////////////////////////////////////////
+var catCup = new CuteKitty("img/cat-cup.jpg");
+var hands = new CuteKitty("img/hands.jpg");
+var hangingWood = new CuteKitty("img/hanging-wood-kitten.jpg");
+var lookingDown =new CuteKitty("img/looking-down.jpg");
+var tiaraKitten =new CuteKitty("img/tiara-kitten.jpg");
+var pinkKitten =new CuteKitty("img/pink-kitten.jpg");
+var starbucksKitten =new CuteKitty("img/starbucks-kitten.jpg");
+var streching =new CuteKitty("img/streching.jpg");
+var plzKitten =new CuteKitty("img/plz-kitten.jpg");
+var cameraKitten =new CuteKitty("img/camera-kitten.jpg");
+var wrapJeans =new CuteKitty("img/wrap-jeans.jpg" );
+var specsKitten =new CuteKitty("img/specs-kitten.jpg");
+//////////////////////////////Random Two Images/////////////////////////////////////////////////////////////////
 
 // Math.floor chops off after decimal
 //Math.rand() - creates 0 to 0.99999999
@@ -42,52 +48,58 @@ var randomImage = function(){
     var rand2 =Math.floor(Math.random() * kittyList.length);
 
     while(rand1 === rand2){
-     rand2 = Math.floor(Math.random() * kittyList.length) +1;
-}
+      rand2 = Math.floor(Math.random() * kittyList.length) +1;
+    }
 
- console.log(rand1);
- console.log(rand2);
+    console.log(rand1);
+    console.log(rand2);
 
     var image1 = document.getElementById("image1");
     var image2 = document.getElementById("image2");
-    console.log(kittyList)
-    image1.innerHTML = kittyList[rand1];
-    image2.innerHTML = kittyList[rand2];
+
+    image1.src = kittyList[rand1].source;
+    image2.src = kittyList[rand2].source;
+
+    image1.index = rand1;
+    image2.index = rand2;
+
+    // image1.innerHTML = kittyList[rand1];
+    // image2.innerHTML = kittyList[rand2];
   }
 //-----------------------------------------------------------------
+// //////////////////////////////Vote/////////////////////////////////////////
+this.addCount = function(event){
+    //console.log(this);
+    //this.arrayOfZeros(source);
+    var urlOfImageClicked = this.src;
+
+    for (var i = 0; i < kittyList.length; i++) {
+      if (urlOfImageClicked.indexOf(kittyList[i].source) !== -1) {
+          kittyList[i].count ++;
+          console.log("Image Clicked: "+ kittyList[i].source + ": Vote Count " + kittyList[i].count);
+      }
+    }
+}
+//console.log(this.addCount);
+
+// var finalArray= function(){
+//   for (var i=0;i<kittyList.length;i++){
+//   return 'Index :'[i] + this.addCount;
+//   };
+
+// console.log(this.finalArray());
 //A reference to the DOM element Node is stored in a variable submit
   var button = document.getElementById("button");
-
-
 // target.event('user_action', function);
 //DOMelement.addEventListener('event',functionName[,Boolean]);
 button.addEventListener('click',randomImage);
 //'click'-The event name is enclosed in quotation marks
+image1.addEventListener('click',this.addCount);
+image2.addEventListener('click',this.addCount);
+
+console.log(arrayOfZeros);
+//console.log('Total count for'+ indexOf(kittyList[i].source+count));
 
 //------------------------------------------------------------------
-// //////////////////////////////Vote/////////////////////////////////////////
-// var carCupCount = 0;
-// var voteCount = function(){
-//   carCupCount = carCupCount +1;
-// }
-//  catCup.addEventListener('click',voteCount);
-//  console.log(carCupCount);
-// // ----------------------------------------------------------
-// var handsCount = 0;
-// var voteCount1 = function(){
-//   handsCount = handsCount +1;
-// }
-//  hands.addEventListener('click',voteCount);
-// // ----------------------------------------------------------
-// var hangingWoodCounts = 0;
-// var voteCount2 = function(){
-//   hangingWoodCounts = hangingWoodCounts +1;
-// }
-//  hands.addEventListener('click',voteCount);
-// // ----------------------------------------------------------
-// var lookingDownCount=0;
-// var voteCount3 = function(){
-//   lookingDownCount = lookingDownCount +1;
-// }
-//  hands.addEventListener('click',voteCount);
-// ----------------------------------------------------------
+
+
